c0534446c33b825ff9c8fa67733e09a10e201d1d
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-02-25 23:47:04 -0500

    builds express server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+const routes = require('./controllers/controller.js');
+
+app.listen(PORT, () => {
+  console.log('server listening on: http://localhost.' + PORT);
+});
