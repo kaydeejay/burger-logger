81f9dafde994d749c06b92fe169f2e595402771f
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-02-26 15:59:57 -0500

    2/4 ORM CRUD methods

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,52 @@
-const connection = require('./connection.js')
+const connection = require('./connection.js');
+
+// Helper function for writing SQL syntax
+const printQuestionMarks = (num) => {
+  const arr = [];
+  for (let i = 0; i < num; i++){
+    arr.push('?');
+  }
+  return arr.toString();
+}
+
+const objToSql = (obj) => {
+  const arr = [];
+  for (let key in obj) {
+    let value = obj[key];
+    if (Object.hasOwnProperty.call(obj,key)) {
+      if (typeof value === 'string' && value.indexOf(' ') >= 0) {
+        value = `'${value}'`;
+      }
+    }
+    arr.push(key + '=' + value);
+  }
+  return arr.toString();
+}
+
+// object for all SQL statement functions
+const orm = {
+  all: (tableInput, cb) => {
+    let queryString = `SELECT * FROM ${tableInput};`;
+    connection.query(queryString, (err,res) => {
+      if (err) {
+        throw err;
+      }
+      cb(res);
+    });
+  },
+  create: (table, cols, vals, cb) => {
+    let queryString = `INSERT INTO ${table} (`;
+
+    queryString += `${cols.toString()}) `;
+    queryString += `VALUES (${printQuestionMarks(vals.length)}) `
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, (err,res) => {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
+};
